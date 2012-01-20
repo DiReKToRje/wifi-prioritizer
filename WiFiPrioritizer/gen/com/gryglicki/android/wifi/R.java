0dd3c398760e0c72d65b6ba279fb7ff802402d4b
hsk3mis <hsk3mis@gmail.com>
2012-01-20 23:25:41 +0100

    menu icons

--- WiFiPrioritizer/gen/com/gryglicki/android/wifi/R.java
+++ WiFiPrioritizer/gen/com/gryglicki/android/wifi/R.java
@@ -18,6 +18,11 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int ic_menu_close_clear_cancel=0x7f020001;
+        public static final int ic_menu_help=0x7f020002;
+        public static final int ic_menu_play_clip=0x7f020003;
+        public static final int ic_menu_preferences=0x7f020004;
+        public static final int ic_menu_stop=0x7f020005;
     }
     public static final class id {
         public static final int button_reconnect=0x7f080003;
